81d98de5b051959582f56f99c9b6ff7338cffd2f
omniofficial <omniofficial04@gmail.com>
2026-02-12 19:20:10 -0800

    Added test folder

--- functions/src/cloudfunctions/hello.ts
+++ functions/src/cloudfunctions/hello.ts
@@ -1,7 +1,11 @@
 import { onCall } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
-const sayHello = onCall((request) => {
-    return { message: "Hello World!" };
+const helloWorld = onCall((req) => {
+    logger.info("Input received:", req.data); // structured log
+    const result = { message: "Hello World!" };
+    logger.info("Returning:", result);
+    return result;
 });
 
-export { sayHello as helloWorld };
+export { helloWorld };
