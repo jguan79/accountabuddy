12c28029ad679c28542ef71cad65f24fd6960092
omniofficial <omniofficial04@gmail.com>
2026-02-15 15:46:52 -0800

    User Schema and folder rework
    Added a user schema to prepare for user login. Reorganized backend

--- functions/src/cloudfunctions/createUser.ts
+++ functions/src/cloudfunctions/createUser.ts
@@ -1,11 +1,11 @@
 import { onCall } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-const helloWorld = onCall((req) => {
-    logger.info("Input received:", req.data); // structured log
+const createUser = onCall((req) => {
+    logger.info("Input received:", req.data);
     const result = { message: "Hello World!" };
     logger.info("Returning:", result);
     return result;
 });
 
-export { helloWorld };
+export { createUser };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import { setGlobalOptions } from "firebase-functions";
 import { onRequest } from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
-import { helloWorld } from "./cloudfunctions/hello";
+import { createUser } from "./cloudfunctions/createUser";
 
 setGlobalOptions({ maxInstances: 10 });
 
-export { helloWorld };
+export { createUser };
