852f36711febf0431827cb6b1dd7d61ff1598723
omniofficial <omniofficial04@gmail.com>
2026-02-15 18:03:04 -0800

    Task Model COMPLETE
    Model for tasks are complete. Included properties

---
+++ functions/src/models/Task.ts
@@ -0,0 +1,9 @@
+// Model for each task. Will hold the values below.
+export interface Task {
+    id: string;
+    subjectTitle: string;
+    dueDate: string;
+    color: string;
+    description: string;
+    userId: string;
+}
